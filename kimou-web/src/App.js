87f250ae13528c4c54a4788dd89800e096eec243
kimou <sylvainkimou@gmail.com>
2025-04-27 21:53:19 +0000

    Nouvelle structure React

---
+++ kimou-web/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Bienvenue sur KIMOU 🚀</h1>
+      <p>Ton application web est en ligne !</p>
+    </div>
+  );
+}
+
+export default App;
